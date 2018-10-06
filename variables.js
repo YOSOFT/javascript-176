4c500cd193a6a2c80c70a6b9e9f034c164e1a15b
YOSOFT <julio.ramirez@yosoft.com.co>
2018-10-06 15:18:12 -0500

    objetos

---
+++ variables.js
@@ -0,0 +1,12 @@
+var x = 5, y , z;
+        var cadena = "dobles"
+        var cadenaSimple = 'simple';
+        var verdadero = true;
+        var arrays = [1, 2, 3]
+
+        console.log(x,y,z);
+        console.log("Numeros",typeof(x))
+        console.log("String",typeof(cadena))
+console.log("String",typeof(cadenaSimple))
+console.log("Boolean",typeof(verdadero))
+console.log("Objeto",typeof(arrays))
